52f1befed2b5affaf48d777eab33adf9ebc00d73
Jussi Seppälä <50781113+joseppala@users.noreply.github.com>
2019-05-26 09:14:48 +0300

    Configure building documentation

---
+++ webpack-common.config.js
@@ -0,0 +1,60 @@
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        exclude: [/node_modules/],
+        loader: 'vue-loader'
+      },
+      // this will apply to both plain `.js` files
+      // AND `<script>` blocks in `.vue` files
+      {
+        test: /\.js$/,
+        exclude: [/node_modules/],
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            [
+              '@babel/preset-env',
+              {
+                targets: {
+                  edge: "44",
+                  firefox: "66",
+                  chrome: "73",
+                  safari: "12",
+                  android: "4.4",
+                  ios: "10",
+                },
+                corejs: "3.0.0",
+                useBuiltIns: "usage",
+              }
+            ]
+          ]
+        }
+      },
+      // this will apply to both plain `.css` files
+      // AND `<style>` blocks in `.vue` files
+      {
+        test: /\.css$/,
+        use: [
+          'vue-style-loader',
+          'css-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src', 'index.html'),
+      filename: 'index.html',
+      chunks: ['main']
+    }),
+    new CleanWebpackPlugin()
+  ],
+};
--- webpack-docs.config.js
+++ webpack-docs.config.js
@@ -2,15 +2,15 @@ const config = require('./webpack-common.config');
 const path = require('path');
 
 module.exports = Object.assign(config, {
-  mode: 'development',
+  mode: 'production',
   entry: {
     'main': path.resolve(__dirname, 'src', 'examples.js'),
   },
   output: {
     filename: '[name].js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'docs'),
   },
   devServer: {
-    contentBase: path.resolve(__dirname, 'dist')
+    contentBase: path.resolve(__dirname, 'docs')
   },
 });
